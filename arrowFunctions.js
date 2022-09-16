0ddf0ad5c295638a0cb260c33477d3f2209fc053
vinay <vinayvarma2545@gmail.com>
2022-09-16 19:07:38 +0530

    arrow_functions

---
+++ arrowFunctions.js
@@ -0,0 +1,26 @@
+// for single line
+
+// multiply = (a,b) => a*b;
+// console.log(multiply(5,8))
+
+
+// for multiple lines use return statement
+
+// arrowFunction2 = (a,b) => {
+//     const c = 10;
+//     return (a+b)*c;
+// }
+// console.log(arrowFunction2(5,2))
+
+
+// arrow function
+
+Mathematics = () => {
+    let x = 5;
+    let y = 6;
+    let z = 1;
+    return(
+        (x*y)+z
+    )
+}
+console.log(Mathematics())
